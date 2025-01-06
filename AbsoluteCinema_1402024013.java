2afff7370adad223e8555ade7d82edb9e3ca5892
azharaul <azharaulia477@gmail.com>
2025-01-06 22:43:29 +0700

    menambahkan menu guest

--- AbsoluteCinema_1402024013.java
+++ AbsoluteCinema_1402024013.java
@@ -9,18 +9,20 @@ public class AbsoluteCinema_1402024013{
         boolean isLogout = false;
         String newFilm;
         String[] arrFilm =  {"5 cm", "Your Name", "Twenty Five Twenty One", "La La Land"};
+        String[] arrGuest = new String[5];
         int[] arrAvailableSit = {50, 50, 50, 50};
         printTheMainTitle("Azhar Aulia Priatna", "1402024013");
         Scanner userInput = new Scanner(System.in);
-        String username, password;
+        // String username, password;
         do { 
             System.out.println("\n=== Login ===");
             System.out.print("Masukan username: ");
             String InputUsername = userInput.next();
-            System.out.print("Masukan password: ");
-            String inputPassword = userInput.next();
-            if(InputUsername.equals("admin") && inputPassword.equals("admin")){
-                String[] arrAdminMenu = {"Lihat Daftar Film", "Tambah Film", "Edit Film", "Hapus Film"};
+            if(InputUsername.equals("admin")){
+                System.out.print("Masukan password: ");
+                String inputPassword = userInput.next();
+                if(inputPassword.equals("admin")){
+                    String[] arrAdminMenu = {"Lihat Daftar Film", "Tambah Film", "Edit Film", "Hapus Film"};
                 do { 
                     printTheAdminMenu(arrAdminMenu);
                     menu = userInput.nextByte();
@@ -28,96 +30,127 @@ public class AbsoluteCinema_1402024013{
                     //daftar menu
                     if(menu == 1){
                         printTheFilmList(arrFilm, arrAvailableSit);
-                    }else if(menu == 2){
-                        System.out.print("Masukan nama film baru: ");
-                        newFilm = new Scanner(System.in).nextLine();
+                        }else if(menu == 2){
+                            System.out.print("Masukan nama film baru: ");
+                            newFilm = new Scanner(System.in).nextLine();
 
-                        //cek apakah indexnya penuh
-                        boolean isFull = true;
-                        for(int i = 0 ; i < arrFilm.length ; i++){
-                            if(arrFilm[i] == null){
-                                isFull = false;
-                                break;
-                            }
-                        }
-
-                        //jika penuh maka ukuran arraynya di resize
-                        if(isFull){
-                            String[] arrFilmTemp = arrFilm;
-                            int[] arrAvailableSitTemp = arrAvailableSit;
-                            arrFilm = new String[arrFilm.length + 1];
-                            arrAvailableSit = new int[arrAvailableSit.length + 1];
-                            for(int i = 0 ; i < arrFilmTemp.length ; i++){
-                                arrFilm[i] = arrFilmTemp[i];
-                                arrAvailableSit[i] = arrAvailableSitTemp[i];
-                            }
-                        }
-
-                        //tambahkan ke posisi yang data array nya null
-                        for(int i = 0 ; i < arrFilm.length ; i++){
-                            if(arrFilm[i] == null){
-                                arrFilm[i] = newFilm;
-                                arrAvailableSit[i] = 50;
-                            }
-                        }
-                        System.out.println("Film " + newFilm + " berhasil ditambahkan.");
-                        isLogout = false;
-
-                    //tambah film
-                    }else if(menu == 3){
-                        printTheFilmList(arrFilm, arrAvailableSit);
-                        System.out.print("Pilih nomor yang ingin diedit: ");
-                        byte number = userInput.nextByte();
-                        System.out.print("Masukan nama baru untuk film: ");
-                        newFilm = new Scanner(System.in).nextLine();
-                        arrFilm[number - 1] = newFilm;
-                        System.out.println("Film berhasil diperbarui.");
-                        isLogout = false;
-
-                    //hapus film
-                    }else if(menu == 4){
-                        printTheFilmList(arrFilm, arrAvailableSit);
-                        System.out.print("Pilih nomor film yang ingin dihapus: ");
-                        byte number = userInput.nextByte();
-                        boolean success = true;
-                        if(number - 1 >= arrFilm.length){
-                            success = false;
-                        }else if(arrFilm[number-1] == null){
-                            success = false;
-                        }else{
-
-                            for(int i = (number - 1) ; i < arrFilm.length ; i++){
-                                if(i == (arrFilm.length - 1)){
-                                    arrFilm[i] = null;
-                                    arrAvailableSit[i] = 0;
-                                }else{
-                                    arrFilm[i] = arrFilm[i + 1];
-                                    arrAvailableSit[i] = arrAvailableSit[i + 1];
+                            //cek apakah indexnya penuh
+                            boolean isFull = true;
+                            for(int i = 0 ; i < arrFilm.length ; i++){
+                                if(arrFilm[i] == null){
+                                    isFull = false;
+                                    break;
                                 }
                             }
 
+                            //jika penuh maka ukuran arraynya di resize
+                            if(isFull){
+                                String[] arrFilmTemp = arrFilm;
+                                int[] arrAvailableSitTemp = arrAvailableSit;
+                                arrFilm = new String[arrFilm.length + 1];
+                                arrAvailableSit = new int[arrAvailableSit.length + 1];
+                                for(int i = 0 ; i < arrFilmTemp.length ; i++){
+                                    arrFilm[i] = arrFilmTemp[i];
+                                    arrAvailableSit[i] = arrAvailableSitTemp[i];
+                                }
+                            }
+
+                            //tambahkan ke posisi yang data array nya null
+                            for(int i = 0 ; i < arrFilm.length ; i++){
+                                if(arrFilm[i] == null){
+                                    arrFilm[i] = newFilm;
+                                    arrAvailableSit[i] = 50;
+                                }
+                            }
+                            System.out.println("Film " + newFilm + " berhasil ditambahkan.");
+                            isLogout = false;
+
+                        //tambah film
+                        }else if(menu == 3){
+                            printTheFilmList(arrFilm, arrAvailableSit);
+                            System.out.print("Pilih nomor yang ingin diedit: ");
+                            byte number = userInput.nextByte();
+                            System.out.print("Masukan nama baru untuk film: ");
+                            newFilm = new Scanner(System.in).nextLine();
+                            arrFilm[number - 1] = newFilm;
+                            System.out.println("Film berhasil diperbarui.");
+                            isLogout = false;
+
+                        //hapus film
+                        }else if(menu == 4){
+                            printTheFilmList(arrFilm, arrAvailableSit);
+                            System.out.print("Pilih nomor film yang ingin dihapus: ");
+                            byte number = userInput.nextByte();
+                            boolean success = true;
+                            if(number - 1 >= arrFilm.length){
+                                success = false;
+                            }else if(arrFilm[number-1] == null){
+                                success = false;
+                            }else{
+
+                                for(int i = (number - 1) ; i < arrFilm.length ; i++){
+                                    if(i == (arrFilm.length - 1)){
+                                        arrFilm[i] = null;
+                                        arrAvailableSit[i] = 0;
+                                    }else{
+                                        arrFilm[i] = arrFilm[i + 1];
+                                        arrAvailableSit[i] = arrAvailableSit[i + 1];
+                                    }
+                                }
+
+                            }
+                            
+                            if(!success){
+                                System.out.println("Film gagal dihapus.");
+                            }else{
+                                System.out.println("Film berhasil dihapus.");
+                            }
+                            isLogout = false;
+                        }else if(menu == 0){
+                            isLogout = true;
+                            System.out.println("Anda telah logout.");
                         }
-                        
-                        if(!success){
-                            System.out.println("Film gagal dihapus.");
-                        }else{
-                            System.out.println("Film berhasil dihapus.");
-                        }
-                        isLogout = false;
-                    }else if(menu == 0){
-                        isLogout = true;
-                        System.out.println("Anda telah logout.");
-                    }
-                } while (menu == 1 || menu == 2 || menu == 3 || menu == 4);
+                    } while (menu == 1 || menu == 2 || menu == 3 || menu == 4);
+                }else{
+                    System.out.println("Password salah.");
+                    isLogout = true;
+                }
+                
+            // }else if(InputUsername.equals("admin") && (!inputPassword.equals("admin"))){
             }else{
-                System.out.println("Password salah.");
-                isLogout = true;
+                //cek apakah slot guestnya sudah penuh
+                boolean isFull = true;
+                for(int i = 0 ; i < arrGuest.length ; i++){
+                    if(arrGuest[i] == null){
+                        isFull = false;
+                        break;
+                    }
+                }
+                //tambahkan ke posisi yang data array nya null
+                for(int i = 0 ; i < arrGuest.length ; i++){
+                    if(arrGuest[i] == null){
+                        arrGuest[i] = InputUsername;
+                    }
+                }
+                if(!isFull){
+                    String[] arrGuestmenu = {"Lihat Daftar Film", "Pesan Kursi"};
+                    do { 
+                        printTheGuestmenu(arrGuestmenu);
+                        menu = userInput.nextByte();
+                        if(menu == 1){
+                            printTheFilmList(arrFilm, arrAvailableSit);
+                        }else if(menu == 2){
+                            printTheFilmList(arrFilm, arrAvailableSit);
+                            System.out.println("Pilih nomor film: ");
+                            byte filmNumber = userInput.nextByte();
+                            
+                        }
+                    } while (menu == 1 || menu == 2);
+                }else{
+                    System.out.println("slot sudah penuh.");
+                }
             }
         } while (isLogout);
-
-
-
-
     }
 
 
@@ -138,6 +171,15 @@ public class AbsoluteCinema_1402024013{
         }
         System.out.print("0. Logout\nPilih menu: ");
     }
+    public static void printTheGuestmenu(String[] arrGuestMenu){
+        System.out.println("\n=== Menu Guest ===");
+        for(int i = 0 ; i < arrGuestMenu.length ; i++){
+            String guestMenu = arrGuestMenu[i];
+            int no = i+1;
+            System.out.println(no + ". " + guestMenu); 
+        }
+        System.out.print("0. Logout\nPilih menu: ");
+    }
     public static void printTheFilmList(String[] arrFilm, int[] arrAvailableSit){
         System.out.println("\n=== Daftar Film ===");
             for(int i = 0 ; i < arrFilm.length ; i++){
@@ -147,7 +189,9 @@ public class AbsoluteCinema_1402024013{
                 if(arrFilm[i] != null && availableSit != 0){
                     System.out.println(no + ". " + filmName + " (Kursi tersedia: " + availableSit + ")");
                 }
-                
             }
+    }
+    public static void printTheAvailableSit(int availableSit){
+        
     }
 }
